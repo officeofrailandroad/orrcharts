2e63c3e07dedc1a602dcf30fe5b9d991485e274f
Oli Elliott <oliver.elliott@orr.gov.uk>
2026-05-12 10:54:49 +0100

    dodge bars if multiple cols supplied

--- R/horizontal_bar.R
+++ R/horizontal_bar.R
@@ -9,6 +9,8 @@
 #'   bars.
 #' @param outside_bar_distance Controls how far outside the bar labels are
 #'   shown. Increase to move further from the bar.
+#' @param legend_position Set location of the legend in the plot. Default is
+#'   bottom right. c(0,0) is bottom left.
 #' @export
 horizontal_bar <- function(
     data,
@@ -17,8 +19,12 @@ horizontal_bar <- function(
     chart_width = 6.7,
     chart_height = 3.567,
     data_labeller = scales::label_number(),
+    bar_colours = orr_colours(),
     outside_bar_threshold = 0.1,
-    outside_bar_distance = 0.05
+    outside_bar_distance = 0.05,
+    show_legend = FALSE,
+    legend_position = c(0.8,0.2)
+
 ) {
   # Check input parameters
   assert_chart_params(
@@ -33,40 +39,59 @@ horizontal_bar <- function(
     msg = "outside_bar_threshold and outside_bar_distance must be single non-negative numbers"
   )
 
-  fixed_data <- data[,1:2]
-  names(fixed_data) <- c("name", "value")
+  fixed_data <- data
+  data_cols <- ncol(fixed_data)
+  names(fixed_data) <- c(
+    "name",
+    # Wrap column names to keep legend compact
+    stringr::str_wrap(names(fixed_data[2:data_cols]), 20)
+  )
+  col_labels <- names(fixed_data)[2:data_cols]
 
   plot_data <- fixed_data %>%
+    tidyr::pivot_longer(- dplyr::all_of("name"), names_to = "col") %>%
     dplyr::mutate(
       name_label = stringr::str_wrap(.data$name, 20),
+      # fix order of dodged bars to match order of columns in data supplied
+      col = factor(col, levels = rev(col_labels)),
       value_label = data_labeller(.data$value),
       value_max = max(.data$value),
       value_min = min(0, .data$value),
-      value_range = .data$value_max - .data$value_min,
+      value_range = .data$value_max - .data$value_min, # width of plot
       label_outside = (abs(.data$value) / .data$value_range < outside_bar_threshold),
+      # Set data label position
       label_y = ifelse(
         .data$label_outside,
         .data$value + outside_bar_distance * .data$value_range,
         .data$value / 2
-        )
+      )
     )
+
+  # Should legend be shown
+  fill_legend = "none"
+  if(show_legend) fill_legend <- "legend"
 
   # Set font family and size
   font_fam <- "Arial"
   showtext::showtext_auto()
   font_size <- 13
 
-  bar_colour <- "#253268"
+  names(bar_colours) <- NULL
 
   bplt <- plot_data %>%
     ggplot2::ggplot(
-      ggplot2::aes(stats::reorder(.data$name_label, .data$value))
+      ggplot2::aes(
+        stats::reorder(.data$name_label, .data$value),
+        fill = .data$col,
+        group = .data$col # important for dodged labels
+      )
     ) +
     ggplot2::geom_col(
       ggplot2::aes(
         y = .data$value
       ),
-      fill = bar_colour
+      position = "dodge",
+      colour = "white" # white space between dodged bars
     ) +
     ggplot2::geom_text(
       ggplot2::aes(
@@ -74,8 +99,8 @@ horizontal_bar <- function(
         y = .data$label_y,
         colour = .data$label_outside
       ),
-      hjust = "centre",
-      vjust = "middle",
+      # position data labels inside each bar
+      position = ggplot2::position_dodge(width = 0.9),
       family = font_fam,
       size = font_size
     ) +
@@ -86,15 +111,28 @@ horizontal_bar <- function(
       expand = c(0, 0)
     ) +
     ggplot2::scale_x_discrete(name = NULL) +
+    ggplot2::scale_fill_manual(
+      name = NULL,
+      values = bar_colours,
+      breaks = col_labels # fixes order of legend to match order of bars
+    ) +
     ggplot2::scale_colour_manual(values = c("white","black")) +
+    ggplot2::guides(colour = "none", fill = fill_legend) +
     ggplot2::theme(
       text = ggplot2::element_text(family = font_fam, size = (font_size * ggplot2::.pt)),
       axis.text = ggplot2::element_text(size = ggplot2::rel(1), lineheight = 0.25),
       axis.text.x = ggplot2::element_blank(),
       axis.line.x = ggplot2::element_blank(),
-      axis.ticks.x = ggplot2::element_blank()
-    ) +
-    ggplot2::guides(colour = "none")
+      axis.ticks.x = ggplot2::element_blank(),
+      legend.position = "inside",
+      legend.direction = "vertical", # Layout legend categories vertical
+      legend.position.inside = legend_position,
+      legend.text = ggplot2::element_text(lineheight = 0.25, size = ggplot2::rel(1)),
+      legend.title = ggplot2::element_blank(), # No legend title
+      # Remove legend background
+      legend.background = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank()
+    )
 
   ggplot2::ggsave(
     filename = filename,
