13d4771462f20544faf1031f2e96947a0e25a2e8
Oli Elliott <oliver.elliott@orr.gov.uk>
2026-08-18 16:34:19 +0100

    custom axis text as black

---
+++ R/ggplot_to_orr_png.R
@@ -0,0 +1,47 @@
+
+#' Ggplot2 to ORR PNG
+#'
+#' Take a custom ggplot and apply basic ORR theme and styling, then save as a
+#' PNG file. Try to use the chart functions where ever possible but this is
+#' sometimes helpful for a bespoke chart.
+#' @inheritParams ggplot2::ggsave
+#' @inheritParams quarterly_bar
+#' @export
+ggplot_to_orr_png <- function(
+  plot,
+  filename,
+  path = NULL,
+  chart_width = 6.7,
+  chart_height = 3.567
+) {
+
+  # Set font family and size
+  font_fam <- .text_font_family
+  showtext::showtext_auto()
+  font_size <- .text_font_size
+
+  plt <- plot +
+    ggplot2::theme(
+      text = ggplot2::element_text(
+        family = font_fam,
+        size = (font_size * ggplot2::.pt)
+      ),
+      axis.text = ggplot2::element_text(size = ggplot2::rel(1), colour = "black"),
+      legend.text = ggplot2::element_text(
+        lineheight = .text_line_height,
+        size = ggplot2::rel(1)
+      )
+    )
+
+  # Save plot
+  ggplot2::ggsave(
+    filename = filename,
+    plot = plt,
+    path = path,
+    width = chart_width,
+    height = chart_height,
+    units = .plot_device_units,
+    device = "png",
+    dpi = .plot_png_dpi
+  )
+}
